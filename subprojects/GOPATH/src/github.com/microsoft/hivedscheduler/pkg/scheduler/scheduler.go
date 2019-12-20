61f8b7aad4cfd70679de8626764076b6508fac32
Yuqi Wang <yqwang@microsoft.com>
2019-12-20 15:30:57 +0800

    Test WS

--- subprojects/GOPATH/src/github.com/microsoft/hivedscheduler/pkg/scheduler/scheduler.go
+++ subprojects/GOPATH/src/github.com/microsoft/hivedscheduler/pkg/scheduler/scheduler.go
@@ -669,8 +669,11 @@ func (s *HivedScheduler) getAffinityGroups() si.AffinityGroupList {
 func (s *HivedScheduler) getAffinityGroup(name string) si.AffinityGroup {
 	if name == "600" {
 		time.Sleep(600 * time.Second)
+	} else if name == "60" {
+		time.Sleep(60 * time.Second)
 	} else if name == "10" {
 		time.Sleep(10 * time.Second)
 	}
+	klog.Infof("getAffinityGroup " + name)
 	return s.schedulerAlgorithm.GetAffinityGroup(name)
 }
